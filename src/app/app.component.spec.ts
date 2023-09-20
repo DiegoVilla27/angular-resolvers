b1eb2c826c4569956c1ffc3af4c8efcfbba0f078
Diego Villanueva <dv270992@gmail.com>
2023-09-20 20:28:34 +0200

    docs: Fix tests in app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe("AppComponent", () => {
   it(`should have as title 'angular-resolvers'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("angular-resolversa");
+    expect(app.title).toEqual("angular-resolvers");
   });
 
   it("should render title", () => {

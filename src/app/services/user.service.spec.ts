8020cfa453f819502cf2d5fbe8caa01116ac81e9
Diego Villa <dv270992@gmail.com>
2023-09-24 14:58:54 +0200

    test: Create test to user service

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,18 +1,32 @@
 import { TestBed } from "@angular/core/testing";
 import { UserService } from "./user.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+  HttpClientTestingModule /* HttpTestingController */
+} from "@angular/common/http/testing";
 
 describe("UserService", () => {
   let service: UserService;
+  //let http: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(UserService);
+    //http = TestBed.inject(HttpTestingController);
   });
 
   it("should be created test", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should call the API with the correct method", () => {
+    /* const req = http.expectOne(`${service.URL}/users?page=0`);
+    req.flush({ data: [{ email: 'dv@gmail.com' }] });
+    tick();
+    console.log(req);
+    expect(req.request.method).toBe('GET');
+    http.verify(); */
     expect(service).toBeTruthy();
   });
 });
